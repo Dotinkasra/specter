6e7761b5b0ee4570e88ea7b30ba635e04408fd73
Dotinkasra <tyekepon@pm.me>
2021-12-30 17:31:43 +0900

    add : macOSの通知へ表示する処理を追加

--- Source/cogs/mute_notification.py
+++ Source/cogs/mute_notification.py
@@ -1,6 +1,8 @@
+import subprocess
+from discord import message
 from discord.ext import commands
 from Source.env.config import Config
-import discord
+from subprocess import PIPE
 
 config = Config()
 admin = config.admin
@@ -20,18 +22,24 @@ class MuteNotification(commands.Cog):
         #通知対象のメンバーではない場合、処理を終了
         if member.id not in notification:
             return
-
+        
+        display_avatar = member.display_avatar.url
         if before.channel is None and after.channel is not None:
-            print(member.display_name + "さんが" + after.channel.name + "に参加しました")
+            title = "参加通知"
+            message = member.display_name + "さんが" + after.channel.name + "に参加しました"
 
         if before.channel is not None and after.channel is None:
-            print(member.display_name + "さんが通話を切断しました")
+            title = "切断通知"
+            message = member.display_name + "さんが通話を切断しました"
 
         if not before.self_mute and after.self_mute:
-            print(member.display_name + "さんがミュートになりました")
+            title = "ミュート通知"
+            message = member.display_name + "さんがミュートになりました"
         
         if before.self_mute and not after.self_mute:
-            print(member.display_name + "さんがミュートを解除しました")
+            title = "ミュート解除通知"
+            message = member.display_name + "さんがミュートを解除しました"
+        subprocess.run(["terminal-notifier", "-title", title, "-message", message, "-contentImage", display_avatar, "-sender", "com.hnc.Discord"])
 
 def setup(bot):
     return bot.add_cog(MuteNotification(bot))
